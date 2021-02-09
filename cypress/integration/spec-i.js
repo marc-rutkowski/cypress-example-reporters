e76f36a6795ec2b81e3b0cb579ba40b710240522
Marc Rutkowski <marc@attractive-media.fr>
2021-02-09 10:20:47 +0100

    duplicate test files to see the MaxEventListeners warning Ref: https://github.com/cypress-io/cypress/issues/1305
    + disable video generation to speed up the test execution

---
+++ cypress/integration/spec-i.js
@@ -0,0 +1,16 @@
+/// <reference types="Cypress" />
+
+describe('Third spec', () => {
+  it('works', () => {
+    cy.wrap('foo').should('equal', 'foo')
+  })
+
+  it('works again', () => {
+    cy.wait(1000)
+      .then(() => {
+        // break on purpose to show failing test report
+        // expect(true).to.be.false
+        expect(true).to.be.true
+      })
+  })
+})
